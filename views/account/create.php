7fdca3cd70453b5a898a724df34e157c49a4ecad
janusonyte <janusonytegabriele@gmail.com>
2023-06-25 15:27:14 +0100

    styling fixed in deposit and withdraw

--- views/account/create.php
+++ views/account/create.php
@@ -1,27 +1,28 @@
 <body>
-    <div class="container">
-        <h1 class="col m-5">Open a new account</h1>
-        <div class="col m-5">
-            <fieldset>
-                <form action="/account/store" method="post">
-                    <div>
-                        <label class="form-label" for="name">First Name</label>
-                        <input class="form-label" type="text" name="name" id="name" required>
-                    </div>
-                    <div>
-                        <label class="form-label" for="lastName">Last Name</label>
-                        <input class="form-label" type="text" name="lastName" id="lastName" required>
-                    </div>
-                    <div>
-                        <label class="form-label" for="personalId">Personal ID</label>
-                        <input class="form-label" type="text" name="personalId" id="personalId" minlength="11"
-                            maxlength="11" required>
-                    </div>
-                    <div class="col m-3">
+    <div class="card">
+        <h5 class="card-header">Open a new account</h5>
+        <div class="card-body">
 
-                        <button class="btn btn-success" type="submit">Open a new account</button>
-                    </div>
-                </form>
-            </fieldset>
+            <form class="row g-3" form action="/account/store" method="post">
+                <div class="col-md-6">
+                    <label for="firstName" class="form-label">First name:</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter your first name">
+                </div>
+                <div class="col-md-6">
+                    <label for="lastName" class="form-label">Last name:</label>
+                    <input type="text" class="form-control" name="lastName" placeholder="Enter your last name">
+                </div>
+                <div class="col-md-6">
+                    <label for="personalId" class="form-label">Personal ID number:</label>
+                    <input type="text" class="form-control" name="personalId" placeholder="Enter your personal ID number">
+                </div>
+
+
+                <div class="col-12">
+                    <button type="submit" class="purple-gradient">Save</button>
+                </div>
+            </form>
+
         </div>
+    </div>
 </body>
